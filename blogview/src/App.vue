037ad30d913eac2e8fe7345d5ff11432826a0275
aaronhua <cjhdwyyx@163.com>
2019-11-29 18:28:53 +0800

    test

--- blogview/src/App.vue
+++ blogview/src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.jpg">
+    <h2>分享 & 技术</h2>
+    <h5>golang & vue</h5>
+
+    <div id="router">
+			<div><router-link  to="/home">首页</router-link></div>
+			<div><router-link  to="/blog">博客</router-link></div>
+			<div><router-link  to="/column">专栏</router-link></div>
+			<div><router-link  to="/open" >开源</router-link></div>
+			<div><router-link  to="/team">团队</router-link></div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,6 +22,9 @@ export default {
 </script>
 
 <style>
+*{
+
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +32,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app>img{
+  width: 200px;
+}
+#router{
+  border-top:1px solid #000;
+  padding-top:10px ;
+  width: 1200px;
+  margin: 0 auto;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+#router div{
+  width: 200px;
+}
+#router a{
+  text-decoration:none;
+  color: black
 }
 </style>
--- blogview/src/router/index.js
+++ blogview/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
+import blogList from '@/components/blogList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component:home
+    },
+    {
+      path: '/blogList',
+      name: 'blogList',
+      component:blogList
+    },
   ]
 })
