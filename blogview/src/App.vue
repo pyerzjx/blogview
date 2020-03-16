e9bc74318439338fc1656e68cddd56ec1a658828
zhang <158180331@qq.com>
2020-03-16 10:52:53 +0800

    update

--- blogview/src/App.vue
+++ blogview/src/App.vue
@@ -1,72 +1,46 @@
 <template>
-  <div id="app">
-    <el-row :gutter="20">
-      <el-col :xs="1" :sm="1" :md="1" :lg="3" :xl="4"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :xs="22" :sm="22" :md="22" :lg="18" :xl="16">
-        <div id="logo"><img src="./assets/logo.jpg"></div>
-        <h2>分享 & 技术</h2>
-        <h5>golang & python</h5>
-        <div id="router">
-          <div><router-link  to="/Home">首页</router-link></div>
-          <div><router-link  to="/Column">专栏</router-link></div>
-          <div><router-link  to="/Blog">博客</router-link></div>
-          <div><router-link  to="/Open" >开源</router-link></div>
-          <div><router-link  to="/Team">团队</router-link></div>
-        </div>
-        <router-view/>
-      </el-col>
-      <el-col :xs="1" :sm="1" :md="1" :lg="3" :xl="4"><div class="grid-content bg-purple"></div></el-col>
-    </el-row>
-  </div>
+  <el-container>
+    <el-header><bheader></bheader></el-header>
+    
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+      
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <script>
+import Bheader from './components/Bheader'
 export default {
-  name: 'App'
-}
+  name: "App",
+  conponents:{
+    bheader:Bheader
+  }
+};
 </script>
 
 <style scoped>
-  .el-col {
-    border-radius: 4px;
-    height: 1000px;
-  }
-  .bg-purple {
-    background: #FFF;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 60px;
 }
-#logo{
-width: 100%
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
 }
-#logo img{
-  margin: 0 auto;
-  height: 160px;
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  min-height: calc(100vh - 120px)
 }
-#router{
-  border-top:1px solid #000;
-  padding-top:10px;
-  width: 100%;
-  margin: 0 auto;
-  display:flex;
-  align-items: center;
-  justify-content: center;
-}
-#router div{
-  width: 100px;
-}
-#router a{
-  text-decoration:none;
-  color: black
-}
+
 </style>
---
+++ blogview/src/components/Bheader.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-menu
+    :default-active="activeIndex2"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">Python</template>
+      <el-menu-item index="2-1">教程</el-menu-item>
+      <el-menu-item index="2-2">博文</el-menu-item>
+    </el-submenu>
+    <el-submenu index="3">
+      <template slot="title">GoLang</template>
+      <el-menu-item index="2-1">教程</el-menu-item>
+      <el-menu-item index="2-2">博文</el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+    name: 'Bheader'
+}
+</script>
